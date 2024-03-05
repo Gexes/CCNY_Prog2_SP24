89689dcf4555355682e01365bed4454412d9517e
Gexes <145797289+Gexes@users.noreply.github.com>
2024-03-05 16:44:01 -0500

    Update - Player can move an jump
    Added raycasting to the player to then hit objects
    Added debugging to test raycast

--- Assets/Scenes/3d game/Script/playerController3D.cs
+++ Assets/Scenes/3d game/Script/playerController3D.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
@@ -9,12 +10,16 @@ public class playerController3D : MonoBehaviour
     public float speedZ;
     private float moveX;
     private float moveZ;
+    public float jumpforce = 50f;
+    public bool canJump;
+    public bool jumped;
+
 
     public float speed = 10f;
     public float lookSpeed = 100f;
     Rigidbody myRB;
     public Camera myCam;
-    public float camLock;
+    public float camLock; //camera up/down
 
     Vector3 myLook;
 
@@ -24,6 +29,8 @@ public class playerController3D : MonoBehaviour
         myRB = GetComponent<Rigidbody>();
         myLook = myCam.transform.forward;
         Cursor.lockState = CursorLockMode.Locked;
+        canJump = true;
+        jumped = false;
     }
     // Update is called once per frame
     void Update()
@@ -36,10 +43,17 @@ public class playerController3D : MonoBehaviour
         myLook += DeltaLook() * Time.deltaTime;
 
         //clamp the magnitude to keep the player from looking fully upside down
-        myLook = Vector3.ClampMagnitude(myLook, camLock);
+        myLook.y = Mathf.Clamp(myLook.y, -camLock, camLock);
 
         transform.rotation = Quaternion.Euler(0f, myLook.x, 0f);
         myCam.transform.rotation = Quaternion.Euler(-myLook.y, myLook.x, 0f);
+
+        //check for key and ability to jump
+        if (Input.GetKey(KeyCode.Space))
+        {
+            jumped = true;
+        }
+        else { jumped = false; }
     }
 
     void FixedUpdate()
@@ -47,8 +61,6 @@ public class playerController3D : MonoBehaviour
         moveX = Input.GetAxis("Horizontal");
         moveZ = Input.GetAxis("Vertical");
 
-        myRB.velocity = new Vector3(moveX * speedX, 0, myRB.velocity.z);
-        myRB.velocity = new Vector3(myRB.velocity.x, 0, moveZ * speedZ);
 
         Vector3 pMove = transform.TransformDirection(Dir());
         myRB.AddForce(pMove * speed * Time.fixedDeltaTime);
@@ -59,6 +71,11 @@ public class playerController3D : MonoBehaviour
 
         //combined velocity of the rigidbody in black
         Debug.DrawRay(transform.position + Vector3.up, myRB.velocity.normalized * 5f, Color.black);
+
+        if (jumped && canJump)
+        {
+            jump();
+        }
     }
 
     Vector3 Dir()
@@ -93,5 +110,33 @@ public class playerController3D : MonoBehaviour
 
         return dLook;
 
+
+
+    }
+    //ad jumpForce and flip boolean for jump request (jumped) to false
+    void jump() 
+    {
+        myRB.AddForce(Vector3.up * jumpforce);
+        jumped = false;
+    }
+    void Kick()
+    {
+        bool rayCast = Physics.Raycast(transform.position, Vector3.forward, 5f);
+        Debug.Log("raycast: " + rayCast);
+        Debug.DrawRay(transform.position, Vector3.forward * 5f, Color.blue);
+
+        if(rayCast)
+        {
+            //code to kick the ball goes here
+        }
+
+    }
+    private void OnCollisionstay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Terrain") { canJump = true; }
+    }
+    private void OnCollisionExit(Collision collision) 
+    {
+        if (collision.gameObject.tag == "Terrain") { canJump = false; }
     }
 }
